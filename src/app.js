e9a3216a67cb0f71c9e7ad93f48803259ef40a9f
btkhoi2001 <bachthienkhoi2001@gmail.com>
2023-02-19 21:41:06 +0700

    Add eslint

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-import express from 'express';
+import express from "express";
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send({
-        status: "success",
-        data: {
-            message: "API is working. Server is running on port 7502"
-        }
-    });
-});
-const PORT = process.env.PORT || 7502;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.status(200).send({
+    status: "success",
+    data: {
+      message: "API is working. Server is running on port 7502",
+    },
   });
+});
 
+const PORT = process.env.PORT || 7502;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
