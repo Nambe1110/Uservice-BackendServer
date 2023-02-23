33542aa3858e081eb336358c798e130a6ae66377
9alaty-coL <trantanloc2111@gmail.com>
2023-02-23 13:30:16 +0700

    Update verify token

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -7,15 +7,15 @@ const verifyToken = async (req, res, next) => {
     return res.status(401).json({ message: "Access token was not provided" });
   }
   const token = bearerHeader.split(" ")[1];
-  const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-  const user = await UserService.getUserById(decoded.id);
 
-  if (!user) {
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    const user = await UserService.getUserById(decoded.id);
+    req.user = user;
+    return next();
+  } catch (error) {
     return res.status(403).json({ message: "Invalid token" });
   }
-
-  req.user = user;
-  return next();
 };
 
 export default verifyToken;
