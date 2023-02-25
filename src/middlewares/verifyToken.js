f98e85fe5e9b2e5d849121fab3283f514c057240
9alaty-coL <trantanloc@gmail.com>
2023-02-25 11:54:48 +0700

    Add auth validators

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
-import UserService from "../components/user/userService.js";
+import UserModel from "../components/user/userModel.js";
 
-const verifyToken = async (req, res, next) => {
+export const verifyToken = async (req, res, next) => {
   const bearerHeader = req.headers.authorization;
   if (bearerHeader == null) {
     return res.status(401).json({ message: "Access token was not provided" });
@@ -10,12 +10,10 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    const user = await UserService.getUserById(decoded.id);
+    const user = await UserModel.findOne({ where: { id: decoded.id } });
     req.user = user;
     return next();
   } catch (error) {
     return res.status(403).json({ message: "Invalid token" });
   }
 };
-
-export default verifyToken;
