625d54b656fd38242242863d097a036bda35b96d
9alaty-coL <trantanloc2111@gmail.com>
2023-03-01 17:29:07 +0700

    Update response error message

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -7,7 +7,7 @@ export const verifyToken = async (req, res, next) => {
   if (bearerHeader == null) {
     return res.status(401).json({
       status: StatusEnum.Error,
-      message: "Access token was not provided",
+      message: "Access token không được cung cấp",
     });
   }
   const token = bearerHeader.split(" ")[1];
@@ -15,11 +15,18 @@ export const verifyToken = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
     const user = await UserService.getUserById(decoded.id);
+    if (user.is_verified === false) {
+      return res.status(403).json({
+        status: StatusEnum.Error,
+        message: "Tài khoản chưa xác thực",
+      });
+    }
     req.user = user;
     return next();
   } catch (error) {
-    return res
-      .status(403)
-      .json({ status: StatusEnum.Error, message: "Invalid token" });
+    return res.status(403).json({
+      status: StatusEnum.Error,
+      message: "Token không hợp lệ hoặc đã hết hạn",
+    });
   }
 };
