dfd39b96709551b4e7bbbdfc90497c54a1d569df
9alaty-coL <trantanloc@gmail.com>
2023-03-03 15:27:41 +0700

    Add api spec for auth

---
+++ src/components/user/userModelSwagger.js
@@ -0,0 +1,34 @@
+// Model Document
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: ID.
+ *           example: 1
+ *         email:
+ *           type: string
+ *           description: User's email.
+ *           example: trantanloc2111@gmail.com
+ *         first_name:
+ *           type: string
+ *           description: User's first name.
+ *           example: Tran
+ *         last_name:
+ *           type: string
+ *           description: User's last name.
+ *           example: Loc
+ *         updatedAt:
+ *           type: timestamp
+ *           description: Last updated date time string.
+ *           example: "2023-03-03T06:30:01.745Z"
+ *         createdAt:
+ *           type: timestamp
+ *           description: Last created date time string.
+ *           example: "2023-03-03T06:30:01.745Z"
+ */
