65244908a23a808d7b9b0fa9bb5304eec7fb74eb
9alaty-coL <trantanloc2111@gmail.com>
2023-03-09 10:25:06 +0700

    Add get company user

--- src/components/user/userApi.js
+++ src/components/user/userApi.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { verifyToken } from "../../middlewares/verifyToken.js";
 import { isOwner } from "../../middlewares/isOwner.js";
-import { getCompanyMembers, changeUserRole } from "./userController.js";
+import {
+  getCompanyMembers,
+  changeUserRole,
+  getUserCompanyById,
+} from "./userController.js";
 
 const userRouter = express.Router({ mergeParams: true });
 
@@ -11,6 +15,7 @@ userRouter.use("/", (req, res, next) => {
 });
 
 userRouter.get("/company-members", verifyToken, getCompanyMembers);
+userRouter.get("/company-members/:userId", verifyToken, getUserCompanyById);
 userRouter.patch("/role", [verifyToken, isOwner], changeUserRole);
 
 export default userRouter;
