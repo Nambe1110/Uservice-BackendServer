79c673c3b807b88d7ce7569328aa8195ebcc0fef
9alaty-coL <trantanloc@gmail.com>
2023-03-19 17:35:36 +0700

    Migrate to node-google-translate-skidz

--- src/modules/Translate.js
+++ src/modules/Translate.js
@@ -1,10 +1,12 @@
-import translate from "translate-google";
+import translate from "node-google-translate-skidz";
 
 export default class Translate {
   static async translate({ text, from, to }) {
-    return translate(text, {
-      from,
-      to,
+    const result = await translate({
+      text: JSON.stringify(text),
+      source: from,
+      target: to,
     });
+    return JSON.parse(result.translation);
   }
 }
