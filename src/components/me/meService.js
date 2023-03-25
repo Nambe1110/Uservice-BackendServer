6658a2f1c2401410f3a978f8ecfdd6fac7e5397a
Nguyễn Hoàng Nam <namqn11102001@gmail.com>
2023-03-25 15:18:27 +0700

    change to randombytes to fix vulnerabilities

--- src/components/me/meService.js
+++ src/components/me/meService.js
@@ -4,9 +4,13 @@ import S3 from "../../modules/S3.js";
 export default class MeService {
   static async changeAvatar({ currentUser, avatar }) {
     const user = await UserModel.findByPk(currentUser.id);
-    const oldAvatar = user.image_url.split(
+    const strArr = user.image_url.split(
       "uservice-internal-s3-bucket.s3.ap-southeast-1.amazonaws.com/avatar/"
-    )[1];
+    );
+    let oldAvatar;
+    if (strArr) {
+      oldAvatar = strArr[strArr.length - 1];
+    }
     // Upload image to S3 and save the image url to DB
     const avatarUrl = await S3.pushMemoryStorageFileToS3(avatar, "avatar");
     user.image_url = avatarUrl;
@@ -14,7 +18,7 @@ export default class MeService {
     delete updatedUser.dataValues.password;
 
     // Delete old image on S3
-    if (oldAvatar !== "uservice-default-user-avatar.png") {
+    if (oldAvatar && oldAvatar !== "uservice-default-user-avatar.png") {
       await S3.removeFromS3(`avatar/${oldAvatar}`);
     }
 
