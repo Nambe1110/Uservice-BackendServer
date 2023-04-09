8df2b95ac507226ae74903a6a81cfeee1b011d11
Nguyễn Hoàng Nam <namqn11102001@gmail.com>
2023-04-09 21:01:10 +0700

    get customer by id api

--- src/components/customer/customerController.js
+++ src/components/customer/customerController.js
@@ -1,5 +1,6 @@
 import CustomerService from "./customerService.js";
 import { StatusType } from "../../constants.js";
+import logger from "../../config/logger/index.js";
 
 export const getCustomers = async (req, res) => {
   const { user } = req;
@@ -15,6 +16,25 @@ export const getCustomers = async (req, res) => {
     return res.status(200).json({
       status: StatusType.SUCCESS,
       data: customers,
+    });
+  } catch (error) {
+    return res
+      .status(error.code ?? 500)
+      .json({ status: StatusType.ERROR, message: error.message });
+  }
+};
+
+export const getCustomerById = async (req, res) => {
+  try {
+    const { customerId } = req.params;
+    const currentUser = req.user;
+    const customer = await CustomerService.getCustomerById({
+      currentUser,
+      customerId,
+    });
+    return res.status(200).json({
+      status: StatusType.SUCCESS,
+      data: customer,
     });
   } catch (error) {
     return res
